493ac972cd55a07aac9dea8297924bf5ac923cdd
Marcin Cieślak <saper@saper.info>
2019-10-17 15:51:29 +0000

    Provide unique filenames for imported components

--- index.js
+++ index.js
@@ -2,40 +2,40 @@ var nodeSass = require('node-sass');
 var path = require('path');
 var fs = require('fs');
 
-var dir_of_origin = {};
+var file_of_origin = {};
 
 var i = 0;
 var importer = function (request, prev, done) {
   if (request) {
-    var fileName = null,
+    var fileName = null, dir_name = null
       buff = "";
 
     console.log(i + ' >> Request: ' + request);
     console.log(i + ' >> Prev: ' + prev);
-    console.log(i + ' >> dir_of_origin: ' + JSON.stringify(dir_of_origin));
-    console.log(i + ' >> dir_of_origin value: ' + dir_of_origin[prev]);
-    if (prev == "stdin") {
-      fileName = path.join(__dirname, 'assets');
-      fileName = path.join(fileName, request);
-    }
-    else {
-      var dirname = dir_of_origin[prev] // #1 with buff
-      // var dirname = path.dirname(prev); // #2 with fileName
-      fileName = path.join(dirname, request);
+    console.log(i + ' >> file_of_origin: ' + JSON.stringify(file_of_origin));
+    console.log(i + ' >> file_of_origin value: ' + file_of_origin[prev]);
+
+    var base_dir = path.join(__dirname, 'assets');
+
+    if (prev in file_of_origin) {
+      dir_name = path.dirname(file_of_origin[prev])
+    } else {
+      dir_name = '';
     }
 
-    if (fileName) {
-      dir_of_origin[request] = path.dirname(fileName);
+    var relative_file_name = path.join(dir_name, request);
+    file_of_origin[request] = relative_file_name;
 
-      fileName += ".scss";
-      buff = fs.readFileSync(fileName, 'utf8');
-    }
+    var full_file_name = path.join(base_dir, relative_file_name + ".scss");
+    console.log(i + ' >> reading file \"' + full_file_name + '\"');
+    console.log(i + ' >> import name will be "' + relative_file_name + '\"');
+    buff = fs.readFileSync(full_file_name, 'utf8');
     i ++;
 
 
     done({
       contents: buff,
-      file: request
+      file: relative_file_name
     });
   } else {
     done();
