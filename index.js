62567c9ae157d463823c992cf631f37ca83d84ab
Marcin Cieślak <saper@saper.info>
2019-10-17 15:23:33 +0000

    map stores directories of origin

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var nodeSass = require('node-sass');
 var path = require('path');
 var fs = require('fs');
 
-var prevMap = {};
+var dir_of_origin = {};
 
 var i = 0;
 var importer = function (request, prev, done) {
@@ -12,20 +12,20 @@ var importer = function (request, prev, done) {
 
     console.log(i + ' >> Request: ' + request);
     console.log(i + ' >> Prev: ' + prev);
-    console.log(i + ' >> PrevMap: ' + JSON.stringify(prevMap));
-    console.log(i + ' >> PrevMap value: ' + prevMap[prev]);
+    console.log(i + ' >> dir_of_origin: ' + JSON.stringify(dir_of_origin));
+    console.log(i + ' >> dir_of_origin value: ' + dir_of_origin[prev]);
     if (prev == "stdin") {
       fileName = path.join(__dirname, 'assets');
       fileName = path.join(fileName, request);
     }
     else {
-      var dirname = path.dirname(prevMap[prev]) // #1 with buff
+      var dirname = dir_of_origin[prev] // #1 with buff
       // var dirname = path.dirname(prev); // #2 with fileName
       fileName = path.join(dirname, request);
     }
 
     if (fileName) {
-      prevMap[request] = fileName;
+      dir_of_origin[request] = path.dirname(fileName);
 
       fileName += ".scss";
       buff = fs.readFileSync(fileName, 'utf8');
